bbf7af506fd4200615dd87baeba1f64ce3d5aae5
peacemakerbak <peacemakerbak@gmail.com>
2024-06-12 12:19:32 -0400

    error fixed

--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,7 +1,9 @@
 // ✨ create your `quotesSlice` in this module
 
+import { createSlice } from '@reduxjs/toolkit'
+
 let id = 1
-const getNextId = () => id++
+export const getNextId = () => id++
 const initialState = {
   displayAllQuotes: true,
   highlightedQuote: null,
@@ -10,7 +12,7 @@ const initialState = {
       id: getNextId(),
       quoteText: "Don't cry because it's over, smile because it happened.",
       authorName: "Dr. Seuss",
-      apocryphal: false,
+      apocryphal: true,
     },
     {
       id: getNextId(),
@@ -26,3 +28,44 @@ const initialState = {
     },
   ],
 }
+
+export const quotesSlice = createSlice({
+  name: 'quotes',
+  initialState,
+  reducers: {
+    toggleVisibility(state) {
+      state.displayAllQuotes = !state.displayAllQuotes
+
+    },
+    deleteQuote(state, action) {
+      state.quotes = state.quotes.filter(qt => qt.id !== action.payload)
+  },
+  editQuoteAuthenticity(state, action) {
+    const quoteToEdit = state.quotes.find(qt => qt.id === action.payload)
+    quoteToEdit.apocryphal = !quoteToEdit.apocryphal
+  },
+  setHighlightedQuote(state, action) {
+    if (state.highlightedQuote === action.payload) {
+      state.highlightedQuote = null
+    } else {
+      state.highlightedQuote = action.payload
+    } 
+
+  },
+  createQuote(state, action) {
+    state.quotes.push(action.payload)
+
+
+  }
+  },
+})
+
+export const {
+  toggleVisibility,
+  deleteQuote,
+  editQuoteAuthenticity,
+  setHighlightedQuote,
+  createQuote
+} = quotesSlice.actions
+
+export default quotesSlice.reducer
